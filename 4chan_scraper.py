a2f4835ecbe69756c12042911b98038beadc3f38
Derick Phan <dtph@ucdavis.edu>
2019-03-30 14:53:22 -0700

    init

---
+++ 4chan_scraper.py
@@ -0,0 +1,79 @@
+from bs4 import BeautifulSoup
+import requests
+import datetime
+import json
+
+from google.cloud import language
+from google.cloud.language import enums
+from google.cloud.language import types
+
+url = 'http://boards.4channel.org'
+headers = {'User-Agent': 'Mozilla/5.0'}
+
+client = language.LanguageServiceClient()
+
+data = []
+
+def generate_pages():
+    pages = []
+
+    page = url + '/a/'
+    pages.append(page)
+
+    for i in range(2, 10):
+        pages.append(page + str(i))
+
+    return pages
+
+def scrape_text(post):
+    d = dict()
+
+    d['text'] = post.select_one('blockquote').text.strip()
+
+    document = types.Document(
+    content=d['text'],
+    type=enums.Document.Type.PLAIN_TEXT)
+
+    sentiment = client.analyze_sentiment(document=document).document_sentiment
+
+    d['sentiment score'] = sentiment.score
+    d['sentiment magnitude'] = sentiment.magnitude
+
+    overall_sentiment += sentiment.score
+    count += 1
+
+    data.append(d)
+
+def main():
+    global overall_sentiment, count
+    pages = generate_pages()
+
+    print("generated page")
+
+    for page in pages:
+        r = requests.get(page,headers=headers)
+        soup = BeautifulSoup(r.content, 'html.parser')
+
+        posts = soup.select('.board .thread .postContainer')
+
+        for post in posts:
+            scrape_text(post)
+
+            print("finished thread")
+
+    overall_sentiment /= count
+
+    with open('board_a.json', 'w') as f:
+        json.dump(data, f)
+
+    data.clear()
+
+    d = dict()
+    d['date'] = datetime.datetime.now()
+    d['overall_sentiment'] = overall_sentiment
+    data.append(d)
+
+    with open('overall_sentiment.json', 'w') as f:
+        json.dump(data, f)
+
+main()
